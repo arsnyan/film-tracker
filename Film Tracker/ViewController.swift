b0a410da15b5640f2335c065e69a9de2a5c256a7
Arsen Saruhanyan <ars.nyan2002@gmail.com>
2025-05-12 23:44:38 +0300

    Initial Commit

---
+++ Film Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Film Tracker
+//
+//  Created by Арсен Саруханян on 12.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
